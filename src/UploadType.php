96d82ac1a9cbc969d7f0bab34f7ebe50d250f46e
Václav Pelíšek <peldax@gmail.com>
2021-05-31 22:28:12 +0200

    Replaced concat with nowdoc

--- src/UploadType.php
+++ src/UploadType.php
@@ -7,8 +7,10 @@ namespace Graphpinator\Upload;
 final class UploadType extends \Graphpinator\Type\ScalarType
 {
     protected const NAME = 'Upload';
-    protected const DESCRIPTION = 'Upload type - represents file which was send to server.'
-    . \PHP_EOL . 'By GraphQL viewpoint it is scalar type, but it must be used as input only.';
+    protected const DESCRIPTION = <<<'NOWDOC'
+    Upload type - represents file which was send to server.
+    By GraphQL viewpoint it is scalar type, but it must be used as input only.;
+    NOWDOC;
 
     public function validateNonNullValue(mixed $rawValue) : bool
     {
